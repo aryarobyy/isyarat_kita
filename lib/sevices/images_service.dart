11953f40e344f7e10d1ad91b199bcf79097da27c
aryarobyy <robyaryanata12345@gmail.com>
2025-01-22 14:26:40 +0700

    Api for images

---
+++ lib/sevices/images_service.dart
@@ -0,0 +1,18 @@
+// import 'package:cloudinary_url_gen/cloudinary.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+//
+// const String IMAGE_COLLECTION = "images";
+//
+// class ImagesService {
+//   late final Cloudinary;
+//
+//   ImagesService() {
+//     if (dotenv.env['CLOUDINARY_CLOUD_NAME'] == null ||
+//         dotenv.env['CLOUDINARY_UPLOAD_PRESET'] == null) {
+//       throw Exception('Cloudinary environment variables not set');
+//     }
+//   }
+//
+//   Future
+// }
